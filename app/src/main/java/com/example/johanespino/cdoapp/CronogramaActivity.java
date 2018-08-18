49a178148bc071d2aa1579359e606a8464b2e92d
johanespino9 <johanespino9@gmail.com>
2018-08-18 05:27:11 -0500

    Activity Menu y Cronograma Agregado y por completar

--- app/src/main/java/com/example/johanespino/cdoapp/CronogramaActivity.java
+++ app/src/main/java/com/example/johanespino/cdoapp/CronogramaActivity.java
@@ -1,17 +1,13 @@
 package com.example.johanespino.cdoapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MenuActivity extends AppCompatActivity {
+public class CronogramaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_cronograma);
     }
-
-
 }
--- app/src/main/java/com/example/johanespino/cdoapp/LoginActivity.java
+++ app/src/main/java/com/example/johanespino/cdoapp/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.johanespino.cdoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void changeAct(View v){
+        Intent i = new Intent(LoginActivity.this, MenuActivity.class);
+        startActivity(i);
     }
 }
